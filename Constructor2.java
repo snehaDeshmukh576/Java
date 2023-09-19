6450dc4f693b10e62355d231708e47c425760e18
Sneha Deshmukh <112338973+snehaDeshmukh576@users.noreply.github.com>
2023-09-19 10:48:27 +0530

    Add files via upload

---
+++ Constructor2.java
@@ -0,0 +1,12 @@
+
+public class Constructor2 {
+	void Constructor2()
+	{
+		System.out.println("welcome");
+	}
+	public static void main(String args[]){
+		Constructor2 C=new Constructor2();
+		C.Constructor2();
+	}
+
+}
