e535f79b779c2a83a14cd06338d7afe7e4b4513a
Sneha Deshmukh <112338973+snehaDeshmukh576@users.noreply.github.com>
2023-09-19 10:49:26 +0530

    Add files via upload

---
+++ Constructor3.java
@@ -0,0 +1,15 @@
+
+public class Constructor3 {
+	
+	 Constructor3()
+	 {
+		 System.out.println("welcome");
+	 }
+	 Constructor3 (int x){
+		 System.out.println(x);
+	 }
+	 public static void main(String args[]){
+		  new Constructor3 ();
+		  new Constructor3 (5);
+	 }
+}
