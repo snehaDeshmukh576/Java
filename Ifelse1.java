fc96567ee0edad2ebdf2878fbc7efcd0f49941b7
Sneha Deshmukh <112338973+snehaDeshmukh576@users.noreply.github.com>
2024-02-23 12:54:07 +0530

    Add files via upload

---
+++ Ifelse1.java
@@ -0,0 +1,12 @@
+
+public class Ifelse1 {
+	
+public static void main(String args[]){
+	int a=0;
+	if(a>0)
+		System.out.println("Positive Number");
+	else
+		System.out.println("Negative Number");
+}
+
+}
