7f131bf3af211434dc75035c0d6fc51e627fadd9
Sneha Deshmukh <112338973+snehaDeshmukh576@users.noreply.github.com>
2024-02-23 12:54:51 +0530

    Add files via upload

---
+++ IfElse2.java
@@ -0,0 +1,11 @@
+
+public class IfElse2 {
+	public static void main(String args[]){
+		int a=5;
+		if(a%2==0)
+			System.out.println("Even Number");
+		else
+			System.out.println("Odd Number");
+	}
+
+}
