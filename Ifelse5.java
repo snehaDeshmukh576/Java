18555f4a4251c65b2306278825c1a8fbd33897c0
Sneha Deshmukh <112338973+snehaDeshmukh576@users.noreply.github.com>
2024-02-24 19:14:25 +0530

    Add files via upload

---
+++ Ifelse5.java
@@ -0,0 +1,15 @@
+
+public class Ifelse5 {
+	public static void main(String args[]){
+		int a=10;
+		if(a>0){
+			System.out.println("Positive Number");
+			System.out.println("End1");}
+			else{
+			System.out.println("Negative Number");
+		System.out.println("End2");
+			}
+		
+	}
+
+}
