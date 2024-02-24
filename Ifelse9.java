4d5e7a3f4a8dc143b04ef5fa648dbdd1c50829b7
Sneha Deshmukh <112338973+snehaDeshmukh576@users.noreply.github.com>
2024-02-24 19:18:45 +0530

    Add files via upload

---
+++ Ifelse9.java
@@ -0,0 +1,14 @@
+
+public class Ifelse9 {
+	public static void main(String args[])
+	{
+		int a=5;
+		if(a-->5)
+			System.out.println(++a);
+		else if(++a<5)
+			System.out.println(a++);
+		else
+			System.out.println(--a);
+	}
+
+}
