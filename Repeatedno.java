8089cec62112b31a40eb1e3e31e9992853eecb02
Sneha Deshmukh <112338973+snehaDeshmukh576@users.noreply.github.com>
2024-02-25 22:09:05 +0530

    Add files via upload

---
+++ Repeatedno.java
@@ -0,0 +1,14 @@
+
+public class Repeatedno {
+public static void main(String args[]){
+		int n=2002;
+		 int count=0;
+		 while(n>0){
+			 int rem=n%10;
+			 if(rem==2){
+				 count++;
+			 }
+			 n=n/10;
+		 }
+}
+}
