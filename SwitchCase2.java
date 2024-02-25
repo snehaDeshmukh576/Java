ca7cdb6d2940e4ccf606c19e5142a446e9fa335a
Sneha Deshmukh <112338973+snehaDeshmukh576@users.noreply.github.com>
2024-02-25 22:10:48 +0530

    Add files via upload

---
+++ SwitchCase2.java
@@ -0,0 +1,15 @@
+
+public class SwitchCase2 {
+	public static void main(String args[]){
+		int a=1;
+		switch(a)
+		{
+		case 1:System.out.println("one");
+		break;
+		case 2:System.out.println("Two");
+		break;
+		default:System.out.println("Invalid");
+		}
+	}
+
+}
