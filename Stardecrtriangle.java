723f52495dc8c38c5c582f180898863aa442485f
Sneha Deshmukh <112338973+snehaDeshmukh576@users.noreply.github.com>
2024-02-27 12:48:22 +0530

    Add files via upload

---
+++ Stardecrtriangle.java
@@ -0,0 +1,12 @@
+
+public class Stardecrtriangle {
+	public static void main(String args[]){
+		for(int i=1;i<=5;i++){
+			for(int j=i;j<=5;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
